cd64990ee0b94846042435554e4e2150fa950035
leongmal <leonik1968@gmail.com>
2025-12-08 20:37:08 +0300

    add tests set_buns, add_ingredient, remove_ingredient

--- conftest.py
+++ conftest.py
@@ -9,3 +9,11 @@ def mock_bun():
     bun.get_name.return_value = "big_bun"
     bun.get_price.return_value = 111
     return bun
+
+@pytest.fixture
+def mock_ingredient():
+    ingredient = Mock()
+    ingredient.get_price.return_value = 121
+    ingredient.get_name.return_value = "x_ingredient"
+    ingredient.get_type.return_value = "FILLING"
+    return ingredient
--- tests/test_burger.py
+++ tests/test_burger.py
@@ -6,3 +6,14 @@ class TestBurger:
         burg = Burger()
         burg.set_buns(mock_bun)
         assert burg.bun == mock_bun
+
+    def test_add_ingredient(self, mock_ingredient):
+        burg = Burger()
+        burg.add_ingredient(mock_ingredient)
+        assert burg.ingredients[0] == mock_ingredient
+
+    def test_remove_ingredient(self, mock_ingredient):
+        burg = Burger()
+        burg.add_ingredient(mock_ingredient)
+        burg.remove_ingredient(0)
+        assert len(burg.ingredients) == 0
